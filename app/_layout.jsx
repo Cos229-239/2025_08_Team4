fc4641d1099b6723297b821c8c74996a7cff68a1
GregHudler <gregory.hudler@outlook.com>
2025-08-11 22:23:52 -0400

    Made index.jsx file title LucidPaths with correct color and font in the _layout.jsx file.  Changed the height to 100 and font size to 40 to match the wireframe.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,42 @@
 // app/_layout.jsx
 import { Tabs, useRouter } from "expo-router";
 import { Ionicons } from '@expo/vector-icons';
-import PlusButton from '../components/Buttons/PlusButton'
+import PlusButton from '../components/Buttons/PlusButton';
+import { useFonts, Pacifico_400Regular } from "@expo-google-fonts/pacifico";
+import { Text } from "react-native";
 
 export default function Layout() {
     const router = useRouter();
+    
+  const [fontsLoaded] = useFonts({
+    Pacifico_400Regular,
+  });
+
   return (
     <Tabs>
-      <Tabs.Screen
+     <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+          headerTitleAlign: "center",
+          headerTitle: () => (
+            <Text
+              style={{
+                fontFamily: "Pacifico_400Regular",
+                fontSize: 40,
+                color: "#FFFFFF",
+                height: 100,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              LucidPaths
+            </Text>
+             
+          ),
+          headerStyle: {
+      backgroundColor: "#004496",
+    },
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="home-outline" color={color} size={size} />
           ),
@@ -38,9 +64,9 @@ export default function Layout() {
       <Tabs.Screen
         name="about"
         options={{
-          title: "About",
+          title: "Menu",
           tabBarIcon: ({ color, size }) => (
-            <Ionicons name="menu-outline" color={color} size={size} />
+            <Ionicons name="menu" color={color} size={size} />
           ),
         }}
       />
