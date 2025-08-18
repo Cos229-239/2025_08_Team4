569540065f78bb8906f213043623a2e213bedb00
Aiden Liriano <aliriano480@gmail.com>
2025-08-18 19:38:19 -0400

    buttons for add and cancel added to the add goal screen(not yet functional)

--- app/addgoal.jsx
+++ app/addgoal.jsx
@@ -1,15 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function AddGoal() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Add Goal Place holder</Text>
-      <Text>This page will have the new goal layout.</Text>
+      <Pressable style={styles.button} onPress={() => console.log("Add button pressed")}>
+        <Text style={styles.buttonText}>Add</Text>
+      </Pressable>
+
+      <Pressable style={[styles.button, styles.cancelButton]} onPress={() => console.log("Cancel button pressed")}>
+        <Text style={styles.buttonText}>Cancel</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", justifyContent: "center" },
-  title: { fontSize: 24, fontWeight: "bold" },
+  container: {
+    flex: 1,
+  },
+  button: {
+    position: 'absolute',
+    alignSelf: 'center',
+    width: 200,
+    paddingVertical: 14,
+    backgroundColor: '#007AFF',
+    borderRadius: 8,
+    bottom: 280,
+    alignItems: 'center',
+  },
+  cancelButton: {
+    bottom: 190,
+    backgroundColor: '#FF3B30',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
